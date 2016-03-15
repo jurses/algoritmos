1670fbac5d73d44244640e6c56d6318d2f3e1497
Raúl Ulises <martin.jurses@gmail.com>
2016-03-15 20:01:37 +0000

    Cambios para la practica 2
    Cambios del vector disperso y espeso(denso).

---
+++ p2/sparse_vector_t.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "pair_t.hpp"
+#include "vector_pair_t.hpp"
+#include "vector_t.hpp"
+
+#include <cmath>
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class sparse_vector_t{
+private:
+	vector_pair_t v_;    // Vector de pares
+	int           sz_;   // Tamaño del vector original
+
+public:
+	sparse_vector_t(void):
+		v_(),
+		sz_(0) {}
+
+	sparse_vector_t(const vector_t& v, double eps):
+		v_(),
+		sz_(v.get_sz()) {
+			
+		// Cuenta el número de elementos no nulos
+		int nz = 0;
+
+		for(int i = 0; i < v.get_sz(); i++)
+			if ( is_not_zero(v.get_v(i), eps) )
+				nz ++;
+
+		v_.resize(nz);
+
+		nz = 0;
+
+		for(int i = 0; i < v.get_sz(); i++)
+			if ( is_not_zero(v.get_v(i), eps) )
+			{
+				v_.get_set_v(nz).set(i, v.get_v(i));
+				nz ++;
+			}		
+
+	}
+
+	double scal_prod(const vector_t& v)const{
+		double aux = 0.0;
+
+		for(int i=0;i < v_.get_sz(); ++i){
+			cout << v_.get_v(i).get_val() << ' ' << i << endl;
+			 aux += v_.get_v(i).get_val() * v.get_v(v_.get_v(i).get_inx());
+		}	
+
+
+		return aux;
+	}
+
+	~sparse_vector_t(void){}
+	
+	istream& read(istream& is){
+		/*
+		int nz;
+
+		is >> nz; //tamaño del vector
+
+		v_.resize(nz);
+		//añadir los elementos al vector v_
+		for(int i=0; i < v_.get_sz(); ++i){
+			if (is_not_zero(is, eps))
+				v_.get_set_v(nz).set(i, is);	
+		}
+		*/
+		is >> sz_;
+
+		v_.read(is);
+
+		return is;
+
+	}
+
+	ostream& write(ostream& os) const{
+	
+		os << "[ " << setw(7) << sz_ << " ";
+		v_.write(os);
+		os << " ]";
+
+		return os;
+	}
+
+private:
+	bool is_not_zero(double val, double eps) { return fabs(val) > eps;}
+
+};
+
